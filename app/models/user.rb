ed27d8c5598c423276abb7a75fa29d2c9de0fbd3
Joshua Zahner <joshuazahner@gmail.com>
2017-07-27 15:08:45 -0500

    Created User<->Stock Association and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+  def can_add_stock?(ticker)
+    return user_stock_limit? && !stock_already_added(ticker)
+  end
+
+  def user_stock_limit?
+    return user_stocks.count < 10
+  end
+
+  def stock_already_added(ticker)
+    stock = Stock.find_by_ticker(ticker)
+    return false unless stock
+    return user_stocks.where(stock_id: stock.id).exists?
+
+
+  end
+
 end
